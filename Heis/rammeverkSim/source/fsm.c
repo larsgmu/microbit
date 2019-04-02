fed56f748ff384700e18a5769f941e7e7eb31230
Lars M <lars.musaus@gmail.com>
2019-04-02 12:50:28 +0200

    added sim

---
+++ Heis/rammeverkSim/source/fsm.c
@@ -0,0 +1,29 @@
+
+
+
+//VARS
+typedef struct elev{
+int state;
+int LastFloor;
+int currentFloor;
+int dir;
+bool** queSys;
+}fsm_vars_t;
+
+
+while(1) {
+
+    if (arrivedAtFloor()) {
+        swich(elev.State){
+            case Moving:
+
+            case DOOR_OPEN:
+
+        }
+    }
+
+    if (doorTimerEnded())
+
+
+
+}
